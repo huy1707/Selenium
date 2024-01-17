f40436439720c68cb681b3c786b3220f4b045ab2
huy1707 <taibistar1707@gmail.com>
2024-01-17 14:36:18 +0700

    Commit update maven

--- src/test/java/DemoSelenium/FirstCode.java
+++ src/test/java/DemoSelenium/FirstCode.java
@@ -2,9 +2,12 @@ package DemoSelenium;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import page.Page1;
 import utils.API;
 import utils.Common;
@@ -21,12 +24,18 @@ public class FirstCode {
 	}
 
 	@Test
+	@Feature("Login")
+	@Story("User logs in with valid credentials")
+    @DisplayName("Test with spaces, expected passed")
 	public void pageLogin() {
 		objPageDemo = new Page1(Common.driver);
 		objPageDemo.loginPageFailed();
 	}
 
 	@Test
+	@Feature("Login")
+	@Story("User logs in with valid api")
+    @DisplayName("Test with API, expected passed")
 	public void testApiCallAndVerifyJson() {
 		API.callAndVerifyResponse();
 	}

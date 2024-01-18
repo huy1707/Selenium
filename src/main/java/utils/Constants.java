77f9fb313aa930109c2d90fe039d4eff04797c9c
huy1707 <taibistar1707@gmail.com>
2024-01-18 09:03:53 +0700

    update constant

--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -2,8 +2,8 @@ package utils;
 
 public class Constants {
 	public static String Login_URL = "https://my.sagaftraplans.org/benmgr/";
-	public static String chromeDriverUrl = "../Selenium/src/main/resources/WebDriver/chromedriver.exe";
-	public static String geckoDriverUrl = "../Selenium/src/main/resources/WebDriver/geckodriver.exe";
+	public static String chromeDriverUrl = "C:/Users/htranba/Desktop/Cypress/Git/Selenium/src/main/resources/WebDriver/chromedriver.exe";
+	public static String geckoDriverUrl = "C:/Users/htranba/Desktop/Cypress/Git/Selenium/src/main/resources/WebDriver/geckodriver.exe";
 	public static int gRetryCount=0;
 	public static int gMaxRetry=3;
 	public static String sRunBrowser = "chrome";
